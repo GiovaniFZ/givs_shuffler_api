009139cd69d345061180969972184d611b6d3db6
GiovaniFZ <giovanifinazzi@gmail.com>
2025-04-13 19:31:12 -0300

    feat: add the possibility to no repeat numbers

--- src/routes/randomNumber.ts
+++ src/routes/randomNumber.ts
@@ -6,22 +6,36 @@ export async function generateRandomNumbers(app: FastifyInstance) {
     max: z.coerce.number(),
     min: z.coerce.number(),
     count: z.coerce.number(),
+    no_repeat: z.coerce.boolean().optional(),
   });
 
   app.get('/random', async (req, res) => {
-    const { max, min, count } = randomRequestSchema.parse(req.query);
+    const { max, min, count, no_repeat } = randomRequestSchema.parse(req.query);
     const minInt = Number(min);
     const maxInt = Number(max);
     const countInt = Number(count);
     const result: number[] = [];
 
     if (minInt > maxInt) {
-      return res.status(400);
+      return res.status(400).send({ error: 'min is greater than max' });
     }
 
-    for (let i = 0; i < countInt; i++) {
-      const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
-      result.push(number);
+    if (no_repeat && countInt > maxInt - minInt + 1) {
+      return res.status(400).send({ error: 'count is greater than the range of numbers' });
+    }
+
+    if (no_repeat) {
+      const uniqueNumbers = new Set<number>();
+      while (uniqueNumbers.size < countInt) {
+        const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
+        uniqueNumbers.add(number);
+      }
+      result.push(...uniqueNumbers);
+    } else {
+      for (let i = 0; i < countInt; i++) {
+        const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
+        result.push(number);
+      }
     }
     res.send({ result });
   });
