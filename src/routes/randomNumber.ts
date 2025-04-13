d4cddeec2b4b5a718ac7bb142d1de8cfee5d5d3a
GiovaniFZ <giovanifinazzi@gmail.com>
2025-04-13 20:57:14 -0300

    ref: numbers: add the possibility to not repeat numbers

--- src/routes/randomNumber.ts
+++ src/routes/randomNumber.ts
@@ -6,7 +6,7 @@ export async function generateRandomNumbers(app: FastifyInstance) {
     max: z.coerce.number(),
     min: z.coerce.number(),
     count: z.coerce.number(),
-    no_repeat: z.coerce.boolean().optional(),
+    no_repeat: z.string(),
   });
 
   app.get('/random', async (req, res) => {
@@ -14,17 +14,18 @@ export async function generateRandomNumbers(app: FastifyInstance) {
     const minInt = Number(min);
     const maxInt = Number(max);
     const countInt = Number(count);
+    const noRepeatBoolean = no_repeat === 'true';
     const result: number[] = [];
 
     if (minInt > maxInt) {
       return res.status(400).send({ error: 'min is greater than max' });
     }
 
-    if (no_repeat && countInt > maxInt - minInt + 1) {
+    if (noRepeatBoolean && countInt > maxInt - minInt + 1) {
       return res.status(400).send({ error: 'count is greater than the range of numbers' });
     }
 
-    if (no_repeat) {
+    if (noRepeatBoolean) {
       const uniqueNumbers = new Set<number>();
       while (uniqueNumbers.size < countInt) {
         const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
