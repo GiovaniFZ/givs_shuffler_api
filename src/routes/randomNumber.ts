7f700dfcc4d8405644f48242ad101d543b47cf46
GiovaniFZ <giovanifinazzi@gmail.com>
2025-04-15 14:19:59 -0300

    ref: numbers: remove some variables, as they are being verified with zod and validate no_repeat better

--- src/routes/randomNumber.ts
+++ src/routes/randomNumber.ts
@@ -6,35 +6,40 @@ export async function generateRandomNumbers(app: FastifyInstance) {
     max: z.coerce.number(),
     min: z.coerce.number(),
     count: z.coerce.number(),
-    no_repeat: z.string(),
+    no_repeat: z
+      .string()
+      .refine((val) => val === 'true' || val === 'false')
+      .transform((val) => val === 'true'),
   });
 
   app.get('/random', async (req, res) => {
     const { max, min, count, no_repeat } = randomRequestSchema.parse(req.query);
-    const minInt = Number(min);
-    const maxInt = Number(max);
-    const countInt = Number(count);
-    const noRepeatBoolean = no_repeat === 'true';
     const result: number[] = [];
 
-    if (minInt > maxInt) {
+    console.log(no_repeat);
+
+    if (min > max) {
       return res.status(400).send({ error: 'min is greater than max' });
     }
 
-    if (noRepeatBoolean && countInt > maxInt - minInt + 1) {
+    if (count <= 0) {
+      return res.status(400).send({ error: 'count must be greater than 0' });
+    }
+
+    if (no_repeat && count > max - min + 1) {
       return res.status(400).send({ error: 'count is greater than the range of numbers' });
     }
 
-    if (noRepeatBoolean) {
+    if (no_repeat) {
       const uniqueNumbers = new Set<number>();
-      while (uniqueNumbers.size < countInt) {
-        const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
+      while (uniqueNumbers.size < count) {
+        const number = Math.floor(Math.random() * (max - min + 1) + min);
         uniqueNumbers.add(number);
       }
       result.push(...uniqueNumbers);
     } else {
-      for (let i = 0; i < countInt; i++) {
-        const number = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
+      for (let i = 0; i < count; i++) {
+        const number = Math.floor(Math.random() * (max - min + 1) + min);
         result.push(number);
       }
     }
