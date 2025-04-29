03abb2135ff855a704a5e32a9a549d1995b4617c
GiovaniFZ <giovanifinazzi@gmail.com>
2025-04-29 13:48:51 -0300

    ref: improve error validation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import fastify from 'fastify';
 import cors from '@fastify/cors';
 import { generateRandomNumbers } from './routes/randomNumber';
 import { generateRandomWords } from './routes/randomWords';
+import { welcomeHTML } from './routes/welcome';
+import { ZodError } from 'zod';
+import { env } from './schema/env';
 
 export const app = fastify();
 
@@ -11,5 +14,21 @@ app.get('/', (_, res) => {
 
 app.register(generateRandomNumbers);
 app.register(generateRandomWords);
+
+app.setErrorHandler((error, _, reply) => {
+  if (env.NODE_ENV != 'production') {
+    console.error(error);
+  }
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: error.format(),
+    });
+  }
+  return reply.status(500).send({
+    message: 'Internal Server Error',
+    error: error.message,
+  });
+});
 
 app.register(cors, {});
